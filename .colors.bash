84a93378b3969177451c16619fcc089c5a2da2b2
rktman <vtbanana@gmail.com>
2015-06-25 21:33:31 -0400

    Bash prompt configuration

---
+++ .colors.bash
@@ -0,0 +1,80 @@
+# Reset
+Color_Off='[0m'       # Text Reset
+
+# Modifiers -- e.g. bold-red would be ${Bold}${Red} or shortcut ${BRed}
+# note: modifiers can be combined -- e.g. ${Bold}${Underline}${Red}
+Bold='[1m'
+Underline='[4m'
+Blink='[5m'  ## doesn't seem to work
+Reverse='[7m'
+Conceal='[8m'
+
+# Regular Colors
+Black='[30m'        # Black
+Red='[31m'          # Red
+Green='[32m'        # Green
+Yellow='[33m'       # Yellow
+Blue='[34m'         # Blue
+Purple='[35m'       # Purple
+Cyan='[36m'         # Cyan
+White='[37m'        # White
+
+# Bold shortcuts
+BBlack='[1;30m'       # Black
+BRed='[1;31m'         # Red
+BGreen='[1;32m'       # Green
+BYellow='[1;33m'      # Yellow
+BBlue='[1;34m'        # Blue
+BPurple='[1;35m'      # Purple
+BCyan='[1;36m'        # Cyan
+BWhite='[1;37m'       # White
+
+# Underline shortcuts
+UBlack='[4;30m'       # Black
+URed='[4;31m'         # Red
+UGreen='[4;32m'       # Green
+UYellow='[4;33m'      # Yellow
+UBlue='[4;34m'        # Blue
+UPurple='[4;35m'      # Purple
+UCyan='[4;36m'        # Cyan
+UWhite='[4;37m'       # White
+
+# Background
+On_Black='[40m'       # Black
+On_Red='[41m'         # Red
+On_Green='[42m'       # Green
+On_Yellow='[43m'      # Yellow
+On_Blue='[44m'        # Blue
+On_Purple='[45m'      # Purple
+On_Cyan='[46m'        # Cyan
+On_White='[47m'       # White
+
+# High Intensity
+IBlack='[90m'       # Black
+IRed='[91m'         # Red
+IGreen='[92m'       # Green
+IYellow='[93m'      # Yellow
+IBlue='[94m'        # Blue
+IPurple='[95m'      # Purple
+ICyan='[96m'        # Cyan
+IWhite='[97m'       # White
+
+# Bold High Intensity shortcuts
+BIBlack='[1;90m'      # Black
+BIRed='[1;91m'        # Red
+BIGreen='[1;92m'      # Green
+BIYellow='[1;93m'     # Yellow
+BIBlue='[1;94m'       # Blue
+BIPurple='[1;95m'     # Purple
+BICyan='[1;96m'       # Cyan
+BIWhite='[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='[100m'   # Black
+On_IRed='[101m'     # Red
+On_IGreen='[102m'   # Green
+On_IYellow='[103m'  # Yellow
+On_IBlue='[104m'    # Blue
+On_IPurple='[105m'  # Purple
+On_ICyan='[106m'    # Cyan
+On_IWhite='[107m'   # White
---
+++ .prompt.bash
@@ -0,0 +1,129 @@
+function git_branch()
+{
+    GIT_BRANCH=""
+    export GIT_BRANCH
+
+    local repo_info rev_parse_exit_code
+    repo_info="$(git rev-parse --git-dir --is-inside-git-dir \
+        --is-bare-repository --is-inside-work-tree \
+        --short HEAD 2>/dev/null)"
+    rev_parse_exit_code="$?"
+
+    if [ -z "$repo_info" ]; then
+        return
+    fi
+
+    local short_sha
+    if [ "$rev_parse_exit_code" = "0" ]; then
+        short_sha="${repo_info##*$'\n'}"
+        repo_info="${repo_info%$'\n'*}"
+    fi
+    local inside_worktree="${repo_info##*$'\n'}"
+    repo_info="${repo_info%$'\n'*}"
+    local bare_repo="${repo_info##*$'\n'}"
+    repo_info="${repo_info%$'\n'*}"
+    local inside_gitdir="${repo_info##*$'\n'}"
+    local g="${repo_info%$'\n'*}"
+
+
+    local r=""
+    local b=""
+    if [ -d "$g/rebase-merge" ]; then
+        read b 2>/dev/null <"$g/rebase-merge/head-name"
+    else
+        if [ -n "$b" ]; then
+            :
+        elif [ -h "$g/HEAD" ]; then
+            # symlink symbolic ref
+            b="$(git symbolic-ref HEAD 2>/dev/null)"
+        else
+            local head=""
+            if ! read head 2>/dev/null <"$g/HEAD"; then
+                return
+            fi
+            # is it a symbolic ref?
+            b="${head#ref: }"
+            if [ "$head" = "$b" ]; then
+                detached=yes
+                b="$(git describe --contains --all HEAD)"
+            fi
+        fi
+    fi
+
+    GIT_BRANCH="${b##refs/heads/}"
+    export GIT_BRANCH
+}
+
+function bash_prompt_command() 
+{
+    #Also in .git-prompt.sh
+
+    # How many characters of the $PWD should be kept
+    local pwdmaxlen=35
+    # Indicate that there has been dir truncation
+    local trunc_symbol=".."
+    local dir=${PWD##*/}
+    pwdmaxlen=$(( ( pwdmaxlen < ${#dir} ) ? ${#dir} : pwdmaxlen ))
+    NEW_PWD=${PWD/#$HOME/\~}
+    local pwdoffset=$(( ${#NEW_PWD} - pwdmaxlen ))
+    if [ ${pwdoffset} -gt "0" ]
+    then
+        NEW_PWD=${NEW_PWD:$pwdoffset:$pwdmaxlen}
+        NEW_PWD=${trunc_symbol}/${NEW_PWD#*/}
+    fi
+    NEW_PWD="${NEW_PWD}"
+    export NEW_PWD
+
+    git_branch
+}
+
+PROMPT_COMMAND=bash_prompt_command
+
+#export GIT_PS1_SHOWDIRTYSTATE=1
+#export GIT_PS1_SHOWCOLORHINTS=1
+#PROMPT_COMMAND='__git_ps1 "[\[[01;34\]m\u@\h\[[0m\]" "]\\\$ "'
+
+# Color chart @ http://wiki.archlinux.org/index.php/Color_Bash_Prompt
+#
+# Powerline font characters:
+# U+E0A0		Version control branch
+# U+E0A1		LN (line) symbol
+# U+E0A2		Closed padlock
+# U+E0B0		Rightwards black arrowhead
+# U+E0B1		Rightwards arrowhead
+# U+E0B2		Leftwards black arrowhead
+# U+E0B3		Leftwards arrowhead
+
+case "${TERM}" in
+    "xterm")
+        TITLEBAR='\[\033]0;\u@\h: ${NEW_PWD}\007\]'
+        # two lines with 'nodes'
+        #export PS1="${TITLEBAR}\[\e[01;37m\]\342\224\214(\[\e[0;94m\]\u@\h\[\e[01;37m\])\$([[ -n \${GIT_BRANCH} ]] && echo \"\342\224\200(\[\e[0;96m\]\${GIT_BRANCH_SYM}\[\e[01;96m\]\${GIT_BRANCH}\[\e[01;37m\])\")\n\342\224\224\342\224\200(\[\e[01;92m\]\${NEW_PWD}\[\e[01;37m\])\342\224\200>\[\e[0m\] "
+
+        # one line with 'nodes'
+        #export PS1="${TITLEBAR}\[$BWhite\]>\342\224\200(\[$IBlue\]\u@\h\[$BWhite\])\$([[ -n \${GIT_BRANCH} ]] && echo \"\342\224\200(\[$ICyan\]\${GIT_BRANCH_SYM}\[$BICyan\]\${GIT_BRANCH}\[$BWhite\])\")\342\224\200(\[$BIGreen\]\${NEW_PWD}\[$BWhite\])\342\224\200>\[$Color_Off\] "
+        # non-unicode
+        #export PS1="${TITLEBAR}\[$BWhite\]>-(\[$IBlue\]\u@\h\[$BWhite\])\$([[ -n \${GIT_BRANCH} ]] && echo \"-(\[$ICyan\]\${GIT_BRANCH_SYM}\[$BICyan\]\${GIT_BRANCH}\[$BWhite\])\")-(\[$BIGreen\]\${NEW_PWD}\[$BWhite\])->\[$Color_Off\] "
+
+        # one line with powerline font 'nodes'
+        # yellow/orange/burgundy
+        #export PS1="${TITLEBAR}\[\e[0;30;48;5;220m\]\u@\h\[\e[0;38;5;220;48;5;202m\]\$([[ -n \${GIT_BRANCH} ]] && echo \"\[\e[0;30;48;5;202m\]\${GIT_BRANCH}\")\[\e[0;38;5;202;48;5;52m\]\[\e[0;97;48;5;52m\]\${NEW_PWD}\[\e[0m\e[0;38;5;52m\]\[$Color_Off\] "
+        # blue/green/grey
+        export PS1="${TITLEBAR}\[\e[0;37;48;5;4m\]\u@\h\[\e[0;38;5;4;48;5;2m\]\$([[ -n \${GIT_BRANCH} ]] && echo \"\[\e[0;38;5;234;48;5;2m\]\${GIT_BRANCH}\")\[\e[0;38;5;2;48;5;0m\]\[\e[0;92;48;5;0m\]\${NEW_PWD}\[\e[0m\e[0;38;5;0m\]\[$Color_Off\] "
+        # blue/green/grey -- no powerline
+        export PS1_NOPL="${TITLEBAR}\[\e[0;37;48;5;4m\]\u@\h\$([[ -n \${GIT_BRANCH} ]] && echo \"\[\e[0;38;5;234;48;5;2m\] \${GIT_BRANCH} \")\[\e[0;92;48;5;0m\]\${NEW_PWD}\[\e[0m\e[0;38;5;0m\]\[$Color_Off\] "
+        ;;
+    "screen")
+        TITLEBAR='\[\033k\u@\h > ${NEW_PWD}\033\\\]'
+        ESC='\[\ek\e\\\]'
+        export PS1="[\[\e[01;34m\]\u@\h \${NEW_PWD}\[\e[0m\]]\$ "
+        ;;
+    *)
+        export PS1='[\[\e[01;34m\]\u@\h \W\[\e[0m\]]\$ '
+    ;;
+esac
+
+
+function nopl {
+    export PS1="$PS1_NOPL"
+}
