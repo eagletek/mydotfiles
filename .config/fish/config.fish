7290c04d4d0e9dc994384ca55ecf5f722bc921f3
Josh O'Connell <ateagletek@gmail.com>
2018-02-09 15:04:03 -0500

    Add fish command timing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,6 @@ set -g fish_key_bindings fish_vi_key_bindings
 set -gx EDITOR vim
 set -gx NUM_CORES (cat /proc/cpuinfo | grep processor | wc -l)
 set -gx PATH . $HOME/bin $PATH
+
+# command timer
+set fish_command_timer_color 999
