5c7991d6cf2b06a28949b502edca0b7200f1289b
Josh O'Connell <ateagletek@gmail.com>
2019-10-22 11:17:22 -0400

    Add cert functions

--- .config/bash/security.bash
+++ .config/bash/security.bash
@@ -8,3 +8,30 @@ function randpass() {
   local length="${1:-16}"
   tr -dc A-Za-z0-9_.\!@#% < /dev/urandom | head -c "${length}";echo;
 }
+
+function gen_self_signed_cert() {
+  local servername="${1:-localhost}"
+  local altname="${servername%%.*}"
+  local santext="[SAN]\nsubjectAltName=DNS:${servername}"
+  if [[ "${altname}" != "${servername}" ]]; then
+    santext="${santext},DNS:${altname}"
+  fi
+  openssl req \
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout ${servername}.key \
+    -new \
+    -out ${servername}.crt \
+    -subj /CN=${servername} \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat /etc/ssl/openssl.cnf \
+            <(printf "${santext}")) \
+    -sha256 \
+    -days 3650
+}
+
+function view_cert() {
+  openssl x509 -in "${1}" -text -noout
+}
